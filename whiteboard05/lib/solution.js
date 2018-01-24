009527695ca04166603450d6ec3ff71d98ecfd93
Victoria Oh <veoh1989@gmail.com>
2018-01-24 13:59:43 -0800

    working on 5

--- whiteboard05/lib/solution.js
+++ whiteboard05/lib/solution.js
@@ -1,18 +1,18 @@
 'use strict';
 
 exports.middle = function (list) {
-  let length = 0;
-  let link = list;
+  let length = 0; //counter, set at zero
+  let link = list; //point at at the current node
 
-  while (link.next !== null) {
-    length++
-    link = link.next;
+  while (link.next !== null) { //while there is a next node
+    length++ //add to counter
+    link = link.next; //move to next node
   }
-    link = list;
-    let half = ~~ (length / 2); //~~ not not
-    while (half !==0) {
-      half--;
-      link = link.next;
+    link = list; //go back to the start
+    let half = ~~ (length / 2); //~~ not not //half of the list?
+    while (half !==0) {//while half is not equal to zero
+      half--; //count down
+      link = link.next; //move to next node
     }
-    return link;
+    return link; //return node
 }
