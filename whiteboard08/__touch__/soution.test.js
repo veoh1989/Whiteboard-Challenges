3654f698d9f40ce206cfe2951ae3b81bc4bb54f3
Victoria Oh <veoh1989@gmail.com>
2018-01-24 15:14:26 -0800

    all tests working

--- whiteboard08/__touch__/soution.test.js
+++ whiteboard08/__touch__/soution.test.js
@@ -10,6 +10,45 @@ describe('Solution Module', () => {
       expect(() => listIntersection(null, new Node(10))).toThrow('Error');
     });
 
-   
+    it('should return null if there are no intersections in the list', () => {
+      let lista = new Node(3);
+      let n = new Node(2);
+      n.next = lista;
+      lista = n;
+      n = new Node(1);
+      n.next = lista;
+      lista = n;
+
+      let listb = new Node(4);
+      n = new Node(5);
+      n.next = listb;
+      listb = n;
+      n = new Node(6);
+      n.next = listb;
+      listb = n;
+
+      expect(listIntersection(lista, listb)).toBeNull();
+    });
+
+    it('should return a new list when there are intersections', () => {
+      let lista = new Node(3);
+      let n = new Node(2);
+      n.next = lista;
+      lista = n;
+      n = new Node(1);
+      n.next = lista;
+      lista = n;
+
+      let listb = new Node(9);
+      n = new Node(8);
+      n.next = listb;
+      listb = n;
+      n = new Node(3);
+      n.next = listb;
+      listb = n;
+
+      let intersection = listIntersection(lista, listb);
+      expect(intersection.value).toBe(3);
+    });
   });
 });
