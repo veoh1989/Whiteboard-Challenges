ee0c8201a360aa3f420962bfe1f9ffd7326d8ef2
Victoria Oh <veoh1989@gmail.com>
2018-01-25 15:07:09 -0800

    done

--- whiteboard05/__test__/solution.test.js
+++ whiteboard05/__test__/solution.test.js
@@ -3,20 +3,25 @@
 const solution = require('../lib/solution');
 
 let nodeObj = {value: 25, next: {value: 1, next: {value: 15, next: {value: 33, next: {value: 4, next: null}}}}};
+let validObj = {next: {next: {next: null, value: 4}, value: 33}, value: 15}
 
 describe('Solution Module', function() {
   describe('#Solution', function() {
-    it('should return 15, next', function() {
-      expect(solution.middle(nodeObj)).toBe(15);
+    it('should return 15 and everything afterwards', function() {
+      expect(solution.middle(nodeObj)).toEqual(validObj);
     });
 
-    // it('should return true if link is a circle', function() {
-    //   nodeObj.next.next = nodeObj;
-    //   expect(solution.circle(nodeObj)).toBe(true);
-    // });
+    it('should return null if list is not an object', function() {
+      expect(solution.middle(nodeObj)).toBeInstanceOf(Object);
+      expect(solution.middle('this is a string')).toBeNull();
+    });
 
-    // it('should return null if not an object', () => {
-    //   expect(solution.circle('this is not an object!')).toBe(null);
-    // });
+    it('link must be a number', () => {
+      expect(solution.middle('this is a string')).toBeNull();
+    });
+
+    it('link numbers cannot be negative', () => {
+      expect(solution.middle(-3)).toBeNull();
+    });
   });
 });
--- whiteboard05/lib/solution.js
+++ whiteboard05/lib/solution.js
@@ -1,10 +1,12 @@
 'use strict';
 
 exports.middle = function (list) {
+  if (typeof list !== 'object') return null;
   let length = 0; //counter, set at zero
   let link = list; //point at at the current node
 
   while (link.next !== null) { //while there is a next node
+    if(typeof link.value !== 'number' || link.value < 0) return null;
     length++ //add to counter
     link = link.next; //move to next node
   }
